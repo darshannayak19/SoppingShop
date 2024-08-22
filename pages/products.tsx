3f65bdbe5f8c891754fb2f433ccaf33f87265378
Darshan Nayak <72485980+darshannayak19@users.noreply.github.com>
2024-08-22 15:29:16 -0400

    updated products page with header

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ProductList from '../components/Products/ProductList';
+import Header from '../components/Header/Header';
 
 const Products: React.FC = () => {
     return (
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="text-3xl font-bold mb-6">Our Products</h1>
-          <ProductList />
-        </div>
+        <>
+          <Header />
+          <div className="container mx-auto px-4 py-8 mt-16"> {/* Added mt-16 for top margin */}
+            <h1 className="text-3xl font-bold mb-6">Our Products</h1>
+            <ProductList />
+          </div>
+        </>
       );
     };
 
